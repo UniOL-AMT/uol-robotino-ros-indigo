79eeefcbda43d1b9fa6aa879f809bde25f04d496
melvinisken <melvin.isken@googlemail.com>
2015-07-01 10:59:54 +0200

    Fixed Digital Outputs and added Relay Control

--- robotino_node/src/DigitalOutputArrayROS.cpp
+++ robotino_node/src/DigitalOutputArrayROS.cpp
@@ -3,9 +3,6 @@
  *
  *  Created on: 09.12.2011
  *      Author: indorewala@servicerobotics.eu
- *	Edited: melvin.isken@uni-oldenburg.de
- *	
- *	Test: rostopic pub -1 /set_digital_values robotino_msgs/DigitalReadings -- '[0, 0]' '[true, false, true, true, false, true, true, true]'
  */
 
 #include "DigitalOutputArrayROS.h"
@@ -24,18 +21,11 @@ DigitalOutputArrayROS::~DigitalOutputArrayROS()
 void DigitalOutputArrayROS::setDigitalValuesCallback( const robotino_msgs::DigitalReadingsConstPtr& msg)
 {
 	int numValues = msg->values.size();
-	if( numValues > 0 && numValues <9)
+	if( numValues > 0 )
 	{
 		int values[numValues];
-		for (int i = 0; i <numValues; i++){
-		values[i]=msg->values.at(i);
-		
-		}
-		//memcpy( values, msg->values.data(), numValues * sizeof(bool) );
+
+		memcpy( values, msg->values.data(), numValues * sizeof(int) );
 		setValues( values, numValues );
-	}
-	else
-	{
-		ROS_WARN("Warning: Digital Output Array out of Bounds: %i", numValues);
 	}
 }
